59632c54ec5f79e42803cd11af155ca952271b0b
Ben Bornebusch <bjb.norf@gmail.com>
2025-05-20 21:51:07 +0200

    fixed the problems

--- wall_e_script.py
+++ wall_e_script.py
@@ -34,18 +34,18 @@ def convert_image(raw_image, resolution):
 
     return img
 
+def get_masked_image(frame, color):
+    func_name = f"find_color_{color}"
+    func = globals().get(func_name)
+    if callable(func):
+        image = func(color)
+        return image
+    else:
+        print(f"No function found for color: {color}")
 #HELPER FUNCTION
 def show_image(image):
     plt.imshow(image)
     plt.show()
-
-
-# HSV range for detecting brown (trash)
-
-
-# Starts simulation
-sim.startSimulation()
-time.sleep(0.5)
 
 def get_image_top():
     sensor_handle_top = top_image_sensor._handle #the top_image_sensor is the object of higher level 
@@ -66,7 +66,14 @@ def get_image_bottom():
     
     return image_bottom
 
-
+def cropping_an_image(image, crop_rows = 10):
+    if image.shape[0] > crop_rows:
+        cropped_result = image[crop_rows:, :, :]
+    else:
+        print("not tall enough")
+        cropped_result = image
+        
+    return cropped_result 
 
 def find_color_green(frame):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -77,19 +84,12 @@ def find_color_green(frame):
     
     result = cv2.bitwise_and(frame, frame, mask = mask)
     
-    crop_rows = 10
-    if result.shape[0] > crop_rows:
-        cropped_result = result[crop_rows:, :, :]
-    else:
-        print("not tall enough")
-        cropped_result = result 
-
     if np.any(mask > 0):
-        return True, cropped_result
+        return True, result
     else:
-        return False, cropped_result
+        return False, result
 
-def find_charching_pat(frame):
+def find_color_yellow(frame):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     YELLOW_LOWER = np.array([20, 100, 100])
     YELLOW_UPPER = np.array([30, 255, 255])
@@ -98,17 +98,10 @@ def find_charching_pat(frame):
     
     result = cv2.bitwise_and(frame, frame, mask = mask)
     
-    crop_rows = 10
-    if result.shape[0] > crop_rows:
-        cropped_result = result[crop_rows:, :, :]
-    else:
-        print("not tall enough")
-        cropped_result = result 
-
     if np.any(mask > 0):
-        return True, cropped_result
+        return True, result
     else:
-        return False, cropped_result
+        return False, result
 
 def find_color_brown(frame):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -117,17 +110,12 @@ def find_color_brown(frame):
 
     mask = cv2.inRange(hsv, BROWN_LOWER, BROWN_UPPER)
     result = cv2.bitwise_and(frame, frame, mask=mask)
-
  
-    crop_rows = 10
-    if result.shape[0] > crop_rows:
-        cropped_result = result[crop_rows:, :, :]
-    else:
-        print("not tall enough")
-        cropped_result = result 
-
     if np.any(mask > 0):
-        return Tru
+        return True, result
+    else:
+        return False, result
+      
 def find_color_black(frame):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
@@ -144,43 +132,79 @@ def find_color_black(frame):
         return True, result_cropped
     else:
         return False, result_cropped
-e, cropped_result
-    else:
-        return False, cropped_result
 
 def going_to_charching():
     bottom_frame = get_image_bottom()
-    found_bottom, yellow_mask_bottom = find_charching_pat(bottom_frame)
+    found_bottom, yellow_mask_bottom = find_color_yellow(bottom_frame)
     if not found_bottom:
         left_motor.run(1)
         right_motor.run(-1)
-            re    
-
-    # Find the horizontal (x) positions of yellow     ls
-    yellow_pixels = np.where(cv2.cvtColor(yellow_mask_bottom, cv2.COLOR_BGR2        urn
-
-    # Use the mean x position of yellow pixels to estimate t    enter
-    cx = int(np.mean(yellowif brown_pixels[1].size > 3300:
+        return
+    
+    # Find the horizontal (x) positions of yellow pixels
+    yellow_pixels = np.where(cv2.cvtColor(yellow_mask_bottom, cv2.COLOR_BGR2GRAY) > 0)
+    if yellow_pixels[1].size == 0:
+        left_motor.run(1)
+        right_motor.run(-1)
+        return
+    if yellow_pixels[1].size > 3300:
         return True
-    else:_p        s[1])) # yellow_pixels[1] gives back the x-ccordinates of the yello        xels, with mean it calculates the average pixel     dinate
-    width = yellow_mask_bottom.shape[1] # the width of the ori    l image
-    center_x = width //         center o    e image
-    tolerance = width // 10  # 10% of im    width            if abs(cx - center_x) <        ce:
-             eft_mot        )
-        right_motor        
-        print(brown_pixels[1].size)            
-    elif cx < c         
-                  ot          )
-                t_moto      1)
-                           left_motor.r                    ight_motor.run(0.5)
-            rn
-
-
-def getting_        dinates            e_to_robot(object:                #position object 
+    else:
+        # Use the mean x position of yellow pixels to estimate the center
+        cx = int(np.mean(yellow_pixels[1])) # yellow_pixels[1] gives back the x-ccordinates of the yellow pixels, with mean it calculates the average pixel coordinate
+        width = yellow_mask_bottom.shape[1] # the width of the original image
+        center_x = width // 2 # center of the image
+        tolerance = width // 10  # 10% of image width
+        print(yellow_pixels[1].size)
+        if abs(cx - center_x) < tolerance:
+            left_motor.run(1)
+            right_motor.run(2)
+            
+        elif cx < center_x:
+            left_motor.run(0.5)
+            right_motor.run(1)
+        else:
+            left_motor.run(1)
+            right_motor.run(0.5)
 
         return False
+
+def going_to_trash_cube():
+    bottom_frame = get_image_bottom()
+    found_bottom, brown_mask_bottom = find_color_brown(bottom_frame)
+    if not found_bottom:
+        left_motor.run(1)
+        right_motor.run(-1)
+        return
     
-    
+    # Find the horizontal (x) positions of yellow pixels
+    brown_pixels = np.where(cv2.cvtColor(brown_mask_bottom, cv2.COLOR_BGR2GRAY) > 0)
+    if brown_pixels[1].size == 0:
+        left_motor.run(1)
+        right_motor.run(-1)
+        return
+    if brown_pixels[1].size > 3300:
+        return True
+    else:
+        # Use the mean x position of yellow pixels to estimate the center
+        cx = int(np.mean(brown_pixels[1])) # yellow_pixels[1] gives back the x-ccordinates of the yellow pixels, with mean it calculates the average pixel coordinate
+        width = brown_mask_bottom.shape[1] # the width of the original image
+        center_x = width // 2 # center of the image
+        tolerance = width // 10  # 10% of image width
+        print(brown_pixels[1].size)
+        if abs(cx - center_x) < tolerance:
+            left_motor.run(1)
+            right_motor.run(2)
+            
+        elif cx < center_x:
+            left_motor.run(0.5)
+            right_motor.run(1)
+        else:
+            left_motor.run(1)
+            right_motor.run(0.5)
+
+        return False
+
 def going_to_compressed_cube():
     bottom_frame = get_image_bottom()
     found_bottom, black_mask_bottom = find_color_black(bottom_frame)
@@ -216,45 +240,42 @@ def going_to_compressed_cube():
             right_motor.run(0.5)
 
         return False
-    sim.getObject(object)
-    pos_ob = sim.getObjectPosition(handle_ob, -1)
     
+def getting_coordinates_relative_to_robot(object):
+    handle_ob = sim.getObject(object)
+    pos_ob = sim.getObjectPosition(handle_ob, -1)
+        
     #position robot
     handle_robot = sim.getObject("/dr12")
     pos_robot = sim.getObjectPosition(handle_robot, -1)
-    
+        
     distance = math.sqrt((pos_ob[0]-pos_robot[0])**2 + (pos_ob[1]-pos_robot[1])**2)
     return distance
 
+def get_camera_views(frame_a, frame_b):
+    combined = np.hstack((frame_a, frame_b))
+    cv2.namedWindow("Combined Camera View", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("Combined Camera View", 1200, 500)
+    cv2.imshow("Combined Camera View", combined)
 
+
+
+# Starts simulation
+sim.startSimulation()
+time.sleep(0.5)
 
 # MAIN LOOP
 while True:
     # Get battery level
     print(f"Battery: {robot.get_battery():.2f}")
 
+    #getting image top and bottom
     image_top = get_image_top()
     image_bottom = get_image_bottom()
     
-    green_mask_bottom = find_color_brown(image_bottom)
-    
-    hallo, brown_mask_top =find_color_brown(image_top)
-    hallo, brown_mask_bottom =find_charching_pat(image_bottom)
-
-
-    image_bottom_cropped = image_bottom[10:, :, :]
-    image_top_cropped = image_top[10:, :, :]
-    
-    
-    combined = np.hstack((image_top_cropped, brown_mask_bottom))
-
-    cv2.namedWindow("Combined Camera View", cv2.WINDOW_NORMAL)
-    cv2.resizeWindow("Combined Camera View", 1200, 500)
-    cv2.imshow("Combined Camera View", combined)
-
+    #stopping the loop
     if cv2.waitKey(1) == ord("q"):
         break
-
     cv2.waitKey(1)
     
     coordinates = getting_coordinates_relative_to_robot("/Charging_Pad")
@@ -272,40 +293,3 @@ while True:
 
 
 
-
-
-if :
-        goinggoing_to_trash_cube())
-        bolbol = goinggoing_to_trash_cube())
-    if bnot bol:bol == FalsTrue:
-        lefleft_motor.run()0
-        righright_motor.rinun()0
-        else:
-        goinggoing_to_trash_cube())
-        robot.ccompress())if robot.getget_battery()) >= 20:
-                                                                                
-                else: 
-        whileWhile while robot.getget_battery()) != 100:
-            coordinates_trash = getting_coordinates_relative_to_robot("/Trash_Can/Trash_Can_Floor")
-    '''
-    if coordinates_trash > 0.29:
-        print(coordinates_trash)
-        going_to_trash()
-    else:
-        left_motor.run(0)
-        right_motor.run(0)                                                                        
-                                                                                                    Charching_Padgcharchingcoordinates_charchingcoordinates_charching5chgoing_to_charching
-            
-    #main  Main function process 55image_bottom_croppedimage_top_cropped
-    robot_battery = robot.getget_battery():.2f
-    print()roborobot_battery0.2.0
-    
-    2020553030
-            trah_detectedctiontrah_detectionif trcompressed_trash_detection = goinggoing_to_compressed_cube())
-            if comprcompressed_trash_detection == True:
-                goinggoing_to_trash())
-                else:
-                goinggoing_to_compressed_cube())
-            while compcompileresscompressed_trash_detection == False:                                                                        
-                    compressed_trash_detection = going_to_compressed_cube()black
-    imageimage_bottomrimage_brown = cropcropping_an_image()imaimageimage_brown, 30image_brownq
